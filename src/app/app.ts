fe8c14cde03510c10a86cd22cda9d8c6a62b39fb
Sofia Gilsanz <softie.gd@gmail.com>
2025-11-28 15:51:04 +0100

    spell card worked on

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SpellCard } from './shared/components/spell-card/spell-card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SpellCard],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('spellbook2eFront');
---
+++ src/app/shared/components/spell-card/spell-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-spell-card',
+  imports: [],
+  templateUrl: './spell-card.html',
+  styleUrl: './spell-card.css',
+})
+export class SpellCard {
+
+}
