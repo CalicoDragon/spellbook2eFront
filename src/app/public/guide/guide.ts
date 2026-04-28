ff032f19cd9c7ed2857d5663e90c9e3f75bfbce2
Sofia Gilsanz <softie.gd@gmail.com>
2026-04-28 20:50:27 +0200

    Created guide base

--- src/app/public/guide/guide.ts
+++ src/app/public/guide/guide.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-guide',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './guide.html',
   styleUrl: './guide.css',
 })
-export class Guide {
-
-}
+export class Guide {}
